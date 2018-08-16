cb33482f8a6ca82f741536c81ed67b9acae0d072
Lucas Coelho <lucascoelhosilvacs@gmail.com>
2018-08-16 13:33:16 -0300

    redis update config

--- src/core/client.redis.js
+++ src/core/client.redis.js
@@ -4,7 +4,11 @@ const redis = require('redis');
 const load = require('./util/load');
 
 const config = load.getRedis();
-const client = redis.createClient(config.port, config.host, config.password);
+const client = redis.createClient({
+  host: config.host,
+  port: config.port,
+  password: config.password,
+});
 
 client.on('connect', () => {
   console.log('===> connected redis');
